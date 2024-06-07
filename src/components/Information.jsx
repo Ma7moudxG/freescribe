fc600e72c1acaddc069b6034975b30f676f370bc
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-06-08 01:07:52 +0300

    third commit

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,8 +6,8 @@ export default function Information(props) {
     const { output, finished } = props
     const [tab, setTab] = useState('transcription')
     const [translation, setTranslation] = useState(null)
-    const [translating, setTranslating] = useState(null)
     const [toLanguage, setToLanguage] = useState('Select language')
+    const [translating, setTranslating] = useState(null)
     console.log(output)
 
     const worker = useRef()
@@ -73,6 +73,8 @@ export default function Information(props) {
     }
 
 
+
+
     return (
         <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 max-w-prose w-full mx-auto'>
             <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
@@ -90,21 +92,14 @@ export default function Information(props) {
                 {tab === 'transcription' ? (
                     <Transcription {...props} textElement={textElement} />
                 ) : (
-                    <Translation {...props} toLanguage={toLanguage} 
-                    translating={translating} textElement={textElement} 
-                    setTranslating={setTranslating} setTranslation={setTranslation} 
-                    setToLanguage={setToLanguage} generateTranslation={generateTranslation} />
+                    <Translation {...props} toLanguage={toLanguage} translating={translating} textElement={textElement} setTranslating={setTranslating} setTranslation={setTranslation} setToLanguage={setToLanguage} generateTranslation={generateTranslation} />
                 )}
             </div>
             <div className='flex items-center gap-4 mx-auto '>
-                <button onClick={handleCopy} title="Copy" 
-                    className='bg-white  hover:text-blue-500 duration-200 
-                    text-blue-300 px-2 aspect-square grid place-items-center rounded'>
+                <button onClick={handleCopy} title="Copy" className='bg-white  hover:text-blue-500 duration-200 text-blue-300 px-2 aspect-square grid place-items-center rounded'>
                     <i className="fa-solid fa-copy"></i>
                 </button>
-                <button onClick={handleDownload} title="Download" 
-                    className='bg-white  hover:text-blue-500 duration-200 
-                    text-blue-300 px-2 aspect-square grid place-items-center rounded'>
+                <button onClick={handleDownload} title="Download" className='bg-white  hover:text-blue-500 duration-200 text-blue-300 px-2 aspect-square grid place-items-center rounded'>
                     <i className="fa-solid fa-download"></i>
                 </button>
             </div>
